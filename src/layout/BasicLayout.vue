de60f4bb03c6a645101a147cad56ead86313fcfe
jssatorres <juansataz.dev@gmail.com>
2023-08-26 13:31:55 +0200

    feat: main menu

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Menu from '../components/MainMenu.vue'
+</script>
+
+<template>
+  <div class="basic-layout">
+    <Menu />
+    <div class="ui container">
+      <slot />
+    </div>
+  </div>
+</template>
