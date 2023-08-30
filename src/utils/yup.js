cfe5a668f9d1e444d578af1105692b368ffe375d
jssatorres <juansataz.dev@gmail.com>
2023-08-30 20:41:43 +0200

    feat: initial yup validation

--- src/utils/yup.js
+++ src/utils/yup.js
@@ -4,3 +4,8 @@ export const schemaRegisterForm = Yup.object().shape({
   email: Yup.string().email(true).required(true),
   password: Yup.string().required(true)
 })
+
+export const schemaLoginForm = Yup.object().shape({
+  username: Yup.string().required(true),
+  password: Yup.string().required(true)
+})
