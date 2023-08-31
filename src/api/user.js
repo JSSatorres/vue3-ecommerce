3ba2e22a0b9a28b0af7791c62e263f3933176e5f
jssatorres <juansataz.dev@gmail.com>
2023-08-31 20:49:06 +0200

    feat: fetch users api

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,8 @@
-import { API_URL } from '../utils/constantsBack'
+import { CONSTANTS } from '../utils/constants'
 
 export async function registerApi(FormData) {
   try {
-    const url = `${API_URL}/api/movies`
+    const url = `${CONSTANTS.API_URL}/api/auth/local/register`
     const params = {
       method: 'POST',
       headers: {
@@ -12,7 +12,27 @@ export async function registerApi(FormData) {
     }
 
     const response = await fetch(url, params)
-    const result = response.json
+    const result = response.json()
+    return result
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export async function loginApi(FormData) {
+  try {
+    const url = `${CONSTANTS.API_URL}/api/auth/local`
+    const params = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(FormData)
+    }
+
+    const response = await fetch(url, params)
+    const result = response.json()
     return result
   } catch (error) {
     console.log(error)
