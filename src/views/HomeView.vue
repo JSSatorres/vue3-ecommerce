374005cc7c0a505016c88993219c75ab326fa51e
jssatorres <juansataz.dev@gmail.com>
2023-09-02 13:21:42 +0200

    feat: fetch get products in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <script setup>
 import BasicLayout from '../layout/BasicLayout.vue'
+import ProductsListing from '../components/ProductsListing.vue'
+import { ref, onMounted } from 'vue'
+import { getProductsApi } from '../api/products'
+
+let products = ref(null)
+
+onMounted(async () => {
+  const response = await getProductsApi(2)
+  products.value = response
+  console.log('dfdsfsdefdsferefrerfreeeeeeeeeeeeeeeeee', products.value)
+})
 </script>
 
 <template>
   <main>
     <BasicLayout>
-      <h3>ostia</h3>
+      <h1>Ultimos Productos</h1>
+      <div class="" v-for="product in products" :key="product.id">
+        <ProductsListing :product="product" />
+      </div>
     </BasicLayout>
   </main>
 </template>
