786ebf41273da5c94b66c80cf429b2ad4d97e378
jssatorres <juansataz.dev@gmail.com>
2023-09-02 13:17:18 +0200

    feat: OrdersView initial

---
+++ src/views/OrdersView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import BasicLayout from '../layout/BasicLayout.vue'
+
+const pepe = ref(null)
+console.log(pepe.value)
+
+onMounted(() => {})
+
+watch()
+</script>
+
+<template>
+  <main>
+    <BasicLayout>
+      <h3>orders</h3>
+    </BasicLayout>
+  </main>
+</template>
